062d4ba08a638d4636b5baebdc3456363eeeaefa
WillamesBarbosa <willamessilva1999@gmail.com>
2026-05-12 06:50:41 -0300

    feat(api): The requests must already include the token in the header. If it is invalid, the refresh token must be triggered, updated, or redirected to the login page.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,47 @@
 import axios from 'axios';
+import { getRefreshToken, getToken, removeTokens, saveTokens } from './auth';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
 });
 
+api.interceptors.request.use((config)=>{
+    const token = getToken()
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+})
+
+api.interceptors.response.use(
+    (response) => response,
+    async(error) =>{
+        const originalRequest = error.config
+
+        if(error.response?.status === 401 && !originalRequest._retry){
+            originalRequest._retry = true
+
+
+            try {
+                const refreshToken = getRefreshToken()
+                const response = await api.post('/refresh', {}, {
+                    headers: {
+                        x_token_refresh: `Refresh ${refreshToken}` 
+                    }
+                })
+
+                saveTokens(response.data.token, response.data.refreshToken)
+
+                return api.request(originalRequest)
+            } catch {
+                removeTokens()
+
+                window.location.href = '/'
+            }
+        }
+
+        return Promise.reject(error)
+    }
+)
 export default api;
