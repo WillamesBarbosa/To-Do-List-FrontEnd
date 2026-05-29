88e0f7c0daef72b0f46ef2517fd5110a83301c2d
WillamesBarbosa <willamessilva1999@gmail.com>
2026-05-29 05:56:05 -0300

    feat(frontend): redirect authenticated users from login to tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
 import Task from './pages/Tasks/Tasks'
 import { PrivateRoute } from '../@/components/PrivateRoute/PrivateRoute'
+import { getToken } from './services/auth'
 
 function App(){
+  const token = getToken()
+
+
   return (
     <Routes>
-      <Route path='/' element={<Login/>} />
+      <Route path='/' element={token ? <Navigate to='/tasks' replace /> : <Login/>} />
         <Route path='/tasks' element={
           <PrivateRoute>
             <Task/>
